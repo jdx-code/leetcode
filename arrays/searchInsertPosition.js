e2035ffd38b6d7f6001446b75235364c90253547
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-05-11 13:18:53 +0530

    Array problem | Difficulty Level - Easy

--- arrays/searchInsertPosition.js
+++ arrays/searchInsertPosition.js
@@ -17,18 +17,35 @@
 
 var searchInsert = function (nums, target) {
     // Method #1 : My Solution
-    let found = 0
-    for(let i=0; i<nums.length; i++){
-        if(nums[i] === target){
-            found = i
-        } else if(nums[i] < target) {
-            found = i+1
-        } 
+    // let found = 0
+    // for(let i=0; i<nums.length; i++){
+    //     if(nums[i] === target){
+    //         found = i
+    //     } else if(nums[i] < target) {
+    //         found = i+1
+    //     } 
+    // }
+    // return found
+
+    // Method #2 : Leetcode solution (Binary search)    
+
+    let low = 0, high = nums.length-1
+    let mid = 0
+    while(low <= high){
+        mid = Math.floor((low+high)/2)
+        if(target === nums[mid]){
+            return mid
+        } else if (target < nums[mid]) {
+            high = mid - 1
+        } else if (target > nums[mid]) {
+            low = mid + 1
+        }
     }
-    return found
+    return low;
+    
 }
 
 console.log(searchInsert([1,3,5,6], 5))
-console.log(searchInsert([1,3,5,6], 2))
+console.log(searchInsert([1,3,5,6], 2)) 
 console.log(searchInsert([1,3,5,6], 4))
 console.log(searchInsert([1,3,5,6], 7))
